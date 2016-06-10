831f26715db45220f2fdb715b326919444a4cf5b
rsl18 <rsluo@zonemail.clpccd.edu>
2016-06-10 12:33:51 -0700

    Init

---
+++ Assets/CustomNetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class CustomNetworkManager : NetworkManager
+{
+    int spawnPosIndex = 0;
+    Transform startPos;
+    public 
+    List<Transform> spawnPosList = new List<Transform>();
+
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GetSpawnPoint();
+
+        var player = (GameObject)GameObject.Instantiate(playerPrefab, startPos.position, startPos.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+        
+
+
+
+    }
+
+    void  GetSpawnPoint()
+    {
+        spawnPosList = startPositions;
+
+        if (playerSpawnMethod == PlayerSpawnMethod.RoundRobin && spawnPosList.Count > 0)
+        {
+            if (spawnPosIndex >= spawnPosList.Count)
+            {
+                spawnPosIndex = 0;
+            }
+​
+            startPos = spawnPosList[spawnPosIndex];
+            spawnPosIndex += 1;
+        }
+    }
+}
